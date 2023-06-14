27d29ca3479c477ae6e3da864cb239fdb9fb6385
Gabriel Riquelme <griquelme.chm@gmail.com>
2023-06-14 16:29:59 -0300

    feat: Update package __init__.

--- src/algoseek_connector/__init__.py
+++ src/algoseek_connector/__init__.py
@@ -1,4 +1,25 @@
+"""
+Provides tools to work with Algoseek Datasets.
+
+Datasets access is done via SQL queries, using either method cascading or raw
+SQL queries.
+
+TODO: list user classes.
+TODO: add examples.
+
+"""
+
 from .sessions import Session, ExecutionError
 from .resources import DataResource, Datagroup, Dataset
+from . import base
+from . import clickhouse
 
-__all__ = ["Session", "ExecutionError", "DataResource", "Datagroup", "Dataset"]
+__all__ = [
+    "base",
+    "Session",
+    "ExecutionError",
+    "DataResource",
+    "Datagroup",
+    "Dataset",
+    "clickhouse",
+]
