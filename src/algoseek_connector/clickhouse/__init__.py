80044576833be28d9ca5f8aacb819ae56c570a61
Gabriel Riquelme <griquelme.chm@gmail.com>
2023-06-21 14:03:30 -0300

    feat: Import resources module.

--- src/algoseek_connector/clickhouse/__init__.py
+++ src/algoseek_connector/clickhouse/__init__.py
@@ -6,5 +6,6 @@ Connector tools for ClickHouse DBMS.
 from . import base
 from . import metadata_api
 from . import sqla_table
+from . import resources
 
-__all__ = ["base", "metadata_api", "sqla_table"]
+__all__ = ["base", "metadata_api", "resources", "sqla_table"]
