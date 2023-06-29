d66166a0c337fe79f2e38c20724207e5fe1d1a24
Gabriel Riquelme <griquelme.chm@gmail.com>
2023-06-29 13:59:52 -0300

    feat: Add fetch and fetch_iter methods.

--- src/algoseek_connector/clickhouse/resources.py
+++ src/algoseek_connector/clickhouse/resources.py
@@ -1,22 +1,53 @@
 """DataResource implementation for ClickHouse DB."""
 
+from __future__ import annotations
+
 import os
 from functools import lru_cache
-from typing import Optional, cast
+from typing import TYPE_CHECKING, Any, Generator, Optional, cast
 
-import numpy as np
 from clickhouse_driver import Client
 from clickhouse_sqlalchemy.drivers.base import ClickHouseDialect
-from pandas import DataFrame
 from sqlalchemy.sql import Select
 
-from ..base import DataGroup, DataResource, DataSet, FunctionHandle
+from ..base import CompiledQuery, DataGroup, DataResource, DataSet, FunctionHandle
 from .base import ColumnMetadata, TableMetadata
-from .sqla_table import SQLAlchemyTableFactory
+from .sqla_table import ClickHouseToNumpyTypeMapper, SQLAlchemyTableFactory
+
+if TYPE_CHECKING:  # pragma: no cover
+    import numpy as np
+    from pandas import DataFrame
 
 
 class ClickHouseDataResource(DataResource):
-    """Manage dataset retrieval from ClickHouse DB."""
+    """
+    Manage dataset retrieval from ClickHouse DB.
+
+    Parameters
+    ----------
+    host : str or None, default=None
+        Host address running a ClickHouse server. If ``None``, the address is
+        set through the environment variable `ALGOSEEK_DATABASE_HOST`.
+    port : int or None, default=None
+        port ClickHouse server is bound to. If ``None``, the port is set
+        through the environment variable `ALGOSEEK_DATABASE_PORT`.
+    user : str or None, default=None
+        Database user. If ``None``, the user is set through the environment
+        variable `ALGOSEEK_DATABASE_USER`.
+    password : str or None, default=None
+        User's password. If ``None``, the password is set through the
+        environment variable `ALGOSEEK_DATABASE_PASSWORD`.
+    use_numpy : bool, default=False
+        If ``True``, retrieve data using numpy types. Otherwise, use Python
+        native types. Numpy type support is limited to `Float32/64`,
+        `[U]Int8/16/32/64`, `Date/DateTime(timezone)/DateTime64(timezone)`,
+        `String/FixedString(N)`, `LowCardinality(T)` and `Nullable(T)`.
+    **kwargs : dict
+        Optional arguments passed to clickhouse_driver.Client constructor.
+        The Client parameters `columnar` and `with_column_types` are always set
+        to ``True``.
+
+    """
 
     def __init__(
         self,
@@ -25,13 +56,18 @@ class ClickHouseDataResource(DataResource):
         password: Optional[str] = None,
         port: Optional[int] = None,
         secure: bool = False,
+        use_numpy: bool = False,
         **kwargs,
     ):
+        settings = kwargs.setdefault("settings", dict())
+        settings["use_numpy"] = use_numpy
+
         self._client = _create_clickhouse_client(
             host, user, password, port, secure, **kwargs
         )
         self._table_metadata_factory = ClickHouseTableMetadataFactory(self._client)
         self._table_factory = SQLAlchemyTableFactory()
+        self._numpy_type_mapper = ClickHouseToNumpyTypeMapper()
         self.groups: dict[str, DataGroup] = dict()
         self._dialect = ClickHouseDialect(paramstyle="pyformat")
 
@@ -40,30 +76,81 @@ class ClickHouseDataResource(DataResource):
         functions = ["sum", "average"]
         return FunctionHandle(functions)
 
-    def fetch(self, stmt: Select, **kwargs):
-        """Execute a Select statement."""
+    def fetch(self, stmt: Select) -> dict[str, Any]:
+        """
+        Retrieve data using a select statement.
+
+        Parameters
+        ----------
+        stmt : Select
+            A select statement generated with :py:meth:`Dataset.select`.
+
+        Returns
+        -------
+        dict[str, tuple]
+            A mapping from column names to values retrieved.
+
+        """
         query = stmt.compile(dialect=self._dialect)
         execute_params = {"with_column_types": True, "columnar": True}
-        if "settings" in kwargs:
-            execute_params["settings"] = kwargs["settings"]
-
         data, names = cast(
-            tuple[list[tuple], list[tuple]],
+            tuple[list, list[tuple[str, str]]],
             self._client.execute(query.string, query.params, **execute_params),
         )
+
         result = dict()
         for column, (name, _) in zip(data, names):
             result[name] = column
         return result
 
+    def fetch_iter(
+        self, stmt: Select, size: int
+    ) -> Generator[dict[str, Any], None, None]:
+        """
+        Retrieve data with result streaming using a select statement.
+
+        Parameters
+        ----------
+        stmt : Select
+            A select statement generated with :py:meth:`Dataset.select`.
+
+        Yields
+        ------
+        dict[str, tuple]
+            A mapping from column names to values retrieved.
+
+        """
+        query = stmt.compile(dialect=self._dialect)
+        execute_params = {"with_column_types": True}
+        iterator = self._client.execute_iter(
+            query.string, query.params, chunk_size=size, **execute_params
+        )
+
+        column_names = None
+        for chunk in iterator:
+            # converts the chunk from a list of rows into a list of columns
+            column_chunk = list(zip(*chunk))
+
+            if column_names is None:
+                # extract column names from the first row of data
+                column_names = [x[0][0] for x in column_chunk]
+                d = {col[0][0]: col[1:] for col in column_chunk}
+            else:
+                d = {k: v for k, v in zip(column_names, column_chunk)}
+
+            yield d
+
     def fetch_dataframe(self, stmt: Select) -> DataFrame:
         """Execute a Select statement and output data as a Pandas DataFrame."""
+        from pandas import DataFrame
+
         return DataFrame(self.fetch_numpy(stmt))
 
     def fetch_numpy(self, stmt: Select) -> dict[str, np.ndarray]:
         """Execute a Select statement and output data as a Pandas DataFrame."""
-        query_kwargs = {"settings": {"use_numpy": True}}
-        return self.fetch(stmt, **query_kwargs)
+        from numpy import array
+
+        return {k: array(v) for k, v in self.fetch(stmt).items()}
 
     def list_groups(self) -> list[str]:
         """List available groups."""
@@ -132,12 +219,10 @@ class ClickHouseDataResource(DataResource):
         table = self._table_factory(table_metadata, data_group.metadata)
         return DataSet(data_group, table, self)
 
-    def compile(self, stmt: Select) -> tuple[str, dict]:
+    def compile(self, stmt: Select) -> CompiledQuery:
         """Convert a stmt into an SQL string."""
         compiled = stmt.compile(dialect=self._dialect)
-        sql = compiled.string
-        params = compiled.params
-        return sql, params
+        return CompiledQuery(compiled.string, compiled.params)
 
 
 class ClickHouseTableMetadataFactory:
