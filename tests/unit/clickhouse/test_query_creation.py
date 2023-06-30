5e54e1493bdf55c35704af6b48d0346d7f632931
Gabriel Riquelme <griquelme.chm@gmail.com>
2023-06-30 15:01:14 -0300

    test: Increase test coverage.

--- tests/unit/clickhouse/test_query_creation.py
+++ tests/unit/clickhouse/test_query_creation.py
@@ -210,3 +210,175 @@ def test_select_where_like(resource):
     actual = remove_new_lines(query.sql)
     assert actual == expected
     assert query.parameters["col5_1"] == pattern
+
+
+def test_select_order(resource):
+    group_name = "g"
+    dataset_name = "t"
+    expected = "SELECT g.t.col1 FROM g.t ORDER BY g.t.col2"
+    dataset = resource.get_dataset(group_name, dataset_name)
+    c = dataset.get_column_handle()
+    stmt = dataset.select(c.col1).order_by(c.col2)
+    query = resource.compile(stmt)
+    actual = remove_new_lines(query.sql)
+    assert actual == expected
+
+
+def test_select_order_desc(resource):
+    group_name = "g"
+    dataset_name = "t"
+    expected = "SELECT g.t.col1 FROM g.t ORDER BY g.t.col2 DESC"
+    dataset = resource.get_dataset(group_name, dataset_name)
+    c = dataset.get_column_handle()
+    stmt = dataset.select(c.col1).order_by(c.col2.desc())
+    query = resource.compile(stmt)
+    actual = remove_new_lines(query.sql)
+    assert actual == expected
+
+
+def test_select_order_multiple(resource):
+    group_name = "g"
+    dataset_name = "t"
+    expected = "SELECT g.t.col1 FROM g.t ORDER BY g.t.col3, g.t.col2 DESC"
+    dataset = resource.get_dataset(group_name, dataset_name)
+    c = dataset.get_column_handle()
+    stmt = dataset.select(c.col1).order_by(c.col3, c.col2.desc())
+    query = resource.compile(stmt)
+    actual = remove_new_lines(query.sql)
+    assert actual == expected
+
+
+def test_select_limit(resource):
+    group_name = "g"
+    dataset_name = "t"
+    # TODO: bug in clickhouse-sqlalchemy extra space in LIMIT. make a PR to fix.
+    expected = "SELECT g.t.col1, g.t.col2 FROM g.t ORDER BY g.t.col1  LIMIT %(param_1)s"
+    dataset = resource.get_dataset(group_name, dataset_name)
+    c = dataset.get_column_handle()
+    limit = 20
+    stmt = dataset.select(c.col1, c.col2).order_by(c.col1).limit(limit)
+    query = resource.compile(stmt)
+    actual = remove_new_lines(query.sql)
+    assert actual == expected
+    assert query.parameters["param_1"] == limit
+
+
+def test_select_arithmetic_add_columns(resource):
+    group_name = "g"
+    dataset_name = "t"
+    col_sum_name = "col_sum"
+    expected = f"SELECT g.t.col1 + g.t.col2 AS {col_sum_name} FROM g.t"
+    dataset = resource.get_dataset(group_name, dataset_name)
+    c = dataset.get_column_handle()
+    stmt = dataset.select((c.col1 + c.col2).label(col_sum_name))
+    query = resource.compile(stmt)
+    actual = remove_new_lines(query.sql)
+    assert actual == expected
+
+
+def test_select_arithmetic_add_literal(resource):
+    group_name = "g"
+    dataset_name = "t"
+    lit = 5.0
+    lit_placeholder = "col1_1"
+    col_label = "col1_sum"
+    expected = f"SELECT g.t.col1 + %({lit_placeholder})s AS {col_label} FROM g.t"
+    dataset = resource.get_dataset(group_name, dataset_name)
+    c = dataset.get_column_handle()
+    stmt = dataset.select((c.col1 + lit).label(col_label))
+    query = resource.compile(stmt)
+    actual = remove_new_lines(query.sql)
+    assert actual == expected
+    assert query.parameters["col1_1"] == lit
+    assert query.parameters[lit_placeholder] == lit
+
+
+def test_select_arithmetic_multiply_columns(resource):
+    group_name = "g"
+    dataset_name = "t"
+    col_label = "col_prod"
+    expected = f"SELECT g.t.col1 * g.t.col2 AS {col_label} FROM g.t"
+    dataset = resource.get_dataset(group_name, dataset_name)
+    c = dataset.get_column_handle()
+    stmt = dataset.select((c.col1 * c.col2).label(col_label))
+    query = resource.compile(stmt)
+    actual = remove_new_lines(query.sql)
+    assert actual == expected
+
+
+def test_select_arithmetic_multiply_literal(resource):
+    group_name = "g"
+    dataset_name = "t"
+    col_label = "col_prod"
+    lit = 5.0
+    lit_placeholder = "col1_1"
+    expected = f"SELECT g.t.col1 * %({lit_placeholder})s AS {col_label} FROM g.t"
+    dataset = resource.get_dataset(group_name, dataset_name)
+    c = dataset.get_column_handle()
+    stmt = dataset.select((c.col1 * lit).label(col_label))
+    query = resource.compile(stmt)
+    actual = remove_new_lines(query.sql)
+    assert actual == expected
+    assert query.parameters[lit_placeholder] == lit
+
+
+def test_select_arithmetic_divide_columns(resource):
+    group_name = "g"
+    dataset_name = "t"
+    col_label = "col_div"
+    expected = f"SELECT g.t.col1 / g.t.col2 AS {col_label} FROM g.t"
+    dataset = resource.get_dataset(group_name, dataset_name)
+    c = dataset.get_column_handle()
+    stmt = dataset.select((c.col1 / c.col2).label(col_label))
+    query = resource.compile(stmt)
+    actual = remove_new_lines(query.sql)
+    assert actual == expected
+
+
+def test_select_arithmetic_divide_literal(resource):
+    group_name = "g"
+    dataset_name = "t"
+    col_label = "col_div"
+    lit_placeholder = "col1_1"
+    lit = 5.0
+    expected = f"SELECT g.t.col1 / %({lit_placeholder})s AS {col_label} FROM g.t"
+    dataset = resource.get_dataset(group_name, dataset_name)
+    c = dataset.get_column_handle()
+    stmt = dataset.select((c.col1 / lit).label(col_label))
+    query = resource.compile(stmt)
+    actual = remove_new_lines(query.sql)
+    assert actual == expected
+
+
+def test_select_complex_query(resource):
+    group_name = "g"
+    dataset_name = "t"
+    having_placeholder = "avg_1"
+    where_placeholder = "toMonth_1"
+    col2_mean_label = "mean_col2"
+    col3_year_label = "year"
+    expected = (
+        f"SELECT avg(g.t.col2) AS {col2_mean_label}, toYear(g.t.col3) AS {col3_year_label} "
+        "FROM g.t "
+        f"WHERE toMonth(g.t.col3) = %({where_placeholder})s "
+        "GROUP BY toYear(g.t.col3) "
+        f"HAVING avg(g.t.col2) > %({having_placeholder})s"
+    )
+    dataset = resource.get_dataset(group_name, dataset_name)
+    c = dataset.get_column_handle()
+    where_value = 1
+    having_value = 1000
+    stmt = (
+        dataset.select(
+            func.avg(c.col2).label(col2_mean_label),
+            func.toYear(c.col3).label(col3_year_label),
+        )
+        .where(func.toMonth(c.col3) == where_value)
+        .group_by(func.toYear(c.col3))
+        .having(func.avg(c.col2) > having_value)
+    )
+    query = resource.compile(stmt)
+    actual = remove_new_lines(query.sql)
+    assert actual == expected
+    assert query.parameters[having_placeholder] == having_value
+    assert query.parameters[where_placeholder] == where_value
