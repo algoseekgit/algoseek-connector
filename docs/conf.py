30dce85305889f9719c50b0fbf814e5590c08446
Gabriel Riquelme <griquelme.chm@gmail.com>
2023-07-06 14:31:15 -0300

    docs: Update sphinx config.

--- docs/conf.py
+++ docs/conf.py
@@ -1,29 +1,30 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+# Project metadata
+# ----------------
 project = "algoseek-connector"
 copyright = "2023, AlgoSeek LLC"
 author = "AlgoSeek LLC"
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# General configuration
+# ---------------------
 
 extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
     "sphinx_copybutton",
     "sphinx_toggleprompt",
+    "numpydoc",
 ]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "sqlalchemy": ("https://docs.sqlalchemy.org/", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+}
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
+# HTML configuration
+# ------------------
 html_theme = "alabaster"
 html_static_path = ["_static"]
