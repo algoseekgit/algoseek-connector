fc7149a1e8c91cfbd4f6369068abf1099f1f1c68
Gabriel Riquelme <griquelme.chm@gmail.com>
2023-08-14 14:33:58 -0300

    feat: Add S3DownloaderClient.

--- src/algoseek_connector/s3/client.py
+++ src/algoseek_connector/s3/client.py
@@ -1,141 +1,412 @@
 """Client protocol for S3 data."""
 
 import datetime
+from functools import lru_cache
+from pathlib import Path
+from typing import Any, Optional, Union
 
-from .. import utils
+from boto3 import Session
 
+from algoseek_connector.base import DataGroupDescription
 
-class PlaceHolders:
-    """Container class for S3 Objects name placeholders."""
+from .. import base
+from ..metadata_api import BaseAPIConsumer
+from . import downloader
 
-    DATE = "yyyymmdd"
-    YEAR = "yyyy"
-    TICKER = "sss"
-    TICKER_START = "s"
-    EXPIRATION_DATE = "expdate"
-    PRODUCT_CODE = "ss"
-    TRADE_CODE = "ssmy"
+BUCKET_GROUPS = "bucket_groups"
+CLOUD_STORAGE = "cloud_storage"
+MAX_DOWNLOAD_SIZE = 1024**12  # maximum total size to download = 1 TiB
 
-    # TODO: make a class iterator to avoid repetition
-    ALL = {"yyyymmdd", "yyyy", "ss", "ssmy", "s", "sss", "expdate"}
-
-
-class S3DataFetcher:
-    """Fetch data from S3 buckets."""
-
-    def __init__(self, bucket_path_format: str, object_path_format: str):
-        prefixes, name = _split_object_path_into_prefixes(object_path_format)
-        name_template, name_placeholders = _make_name_template(name)
-        prefix_template, prefix_placeholders = _make_prefix_template(prefixes)
-        self.name = name_template
-        self.prefix = prefix_template
-        self.name_placeholders = name_placeholders
-        self.prefix_placeholders = prefix_placeholders
-
-    def _create_prefixes(self, **kwargs):
-        prefixes = dict()
-        for placeholder, values in kwargs.items():
-            if placeholder == PlaceHolders.DATE:
-                prefixes[placeholder] = _make_list_of_date_prefixes(values)
-            elif placeholder == "s":
-                pass
+date_like = Union[datetime.date, str]
 
 
-def _make_list_of_tickers(values) -> list[str]:
-    if isinstance(values, str):
-        tickers = [values]
-    elif isinstance(values, list) and all(isinstance(x, str) for x in values):
-        tickers = values
-    else:
-        msg = f"{values} is not a valid specification for Tickers filter."
-        raise ValueError(msg)
-    return tickers
+class S3DownloaderClient(base.ClientProtocol):
+    """ClientProtocol for downloading files from S3."""
+
+    def __init__(self, session: Session, api: BaseAPIConsumer):
+        self.api = api
+        self._bucket_metadata = BucketMetadataProvider(api)
+        self._file_downloader = downloader.FileDownloader(session)
+
+    def compile(self, stmt):  # pragma: no cover
+        """Compile a SQLAlchemy Select statement into a CompiledQuery."""
+        raise NotImplementedError
+
+    def download(
+        self,
+        dataset_text_id: str,
+        download_path: Path,
+        date: Union[date_like, tuple[date_like, date_like]],
+        symbols: Union[str, list[str]],
+        expiration_date: Optional[Union[date_like, tuple[date_like, date_like]]] = None,
+    ):
+        """
+        Download data from the dataset.
+
+        Parameters
+        ----------
+        dataset_text_id : str
+            The dataset text id.
+        download_path : pathlib.Path
+            Path to a directory to download dataset files.
+        date : str, datetime.date or tuple
+            Download data in this date range. Dates can be passed as a str with
+            `yyyymmdd` format or as date objects. If a tuple is passed, it is
+            interpreted as a date range and all dates in the closed interval
+            between the two dates are generated. I a single date is passed,
+            download data from this specific date.
+        symbols : str or list[str]
+            Download data associated with these symbols.
+        expiration date : str, datetime.date or tuple
+            Download data with expiration dates in this date range. Dates must
+            be passed used the same format used for the `date` parameter.
+        n_jobs : int or None, default=None
+            The number of jobs created to download files in a parallel manner.
+            If ``None``, a single worker is used.
+
+        """
+        # Creates a dataset downloader using an independent copy.
+        # boto3 sessions are not thread/multi process safe, so  this allows to
+        # download multiple datasets at the same.
+        dataset_downloader = S3DatasetDownloader(
+            self._file_downloader.copy(), self._bucket_metadata
+        )
+        dataset_downloader.download(
+            dataset_text_id, download_path, date, symbols, expiration_date
+        )
+
+    def get_dataset_columns(
+        self, group: str, dataset: str
+    ) -> base.DataSetDescription:  # pragma: no cover
+        """Create a dataset."""
+        raise NotImplementedError
+
+    def create_function_handle(self):  # pragma: no cover
+        """Create a FunctionHandle instance."""
+        raise NotImplementedError
+
+    def fetch(self, query, **kwargs):  # pragma: no cover
+        """Fetch a select query."""
+        raise NotImplementedError
+
+    def fetch_iter(self, query, size: int, **kwargs):  # pragma: no cover
+        """Yield a select query in chunks."""
+        raise NotImplementedError
+
+    def fetch_dataframe(self, query, **kwargs):  # pragma: no cover
+        """Fetch a select query and output results as a Pandas DataFrame."""
+        raise NotImplementedError
+
+    def fetch_iter_dataframe(self, query, size: int, **kwargs):  # pragma: no cover
+        """Yield a select query in chunks, using pandas DataFrames."""
+        raise NotImplementedError
+
+    def list_datagroups(self) -> list[str]:
+        """List available data groups."""
+        return self.api.list_datagroups()
+
+    @lru_cache
+    def list_datasets(self, group_text_id: str) -> list[str]:
+        """List available data groups."""
+        # datasets are listed based on the following conditions:
+        # Must have at least a bucket group and csv column information
+        group_id = self.api.get_datagroup_metadata(group_text_id)["id"]
+        datasets = list()
+        for text_id in self.api.list_datasets():
+            dataset_metadata = self.api.get_dataset_metadata(text_id)
+            if dataset_metadata["datagroup_id"] != group_id:
+                continue
+
+            cloud_metadata = dataset_metadata[CLOUD_STORAGE]
+            if cloud_metadata is None:
+                continue
+
+            bucket_groups = cloud_metadata[BUCKET_GROUPS]
+            has_csv_columns = len(cloud_metadata["csv_columns"]) > 0
+            has_bucket_groups = isinstance(bucket_groups, list) and len(bucket_groups)
+            is_valid_dataset = has_csv_columns and has_bucket_groups
+
+            if is_valid_dataset:
+                datasets.append(text_id)
+        return datasets
 
 
-def _make_list_of_date_prefixes(values) -> list[str]:
-    if isinstance(values, tuple):
-        start_date = _normalize_date(values[0])
-        end_date = _normalize_date(values[1])
-    elif isinstance(values, (datetime.date, str)):
-        start_date = _normalize_date(values)
-        end_date = start_date
-    else:
-        msg = "{values} is not a valid specification for date filter."
-        raise ValueError(msg)
-    date_range = utils.iterate_date_range(start_date, end_date)
-    return list(x.strftime("%Y%m%d") for x in date_range)
+class S3DescriptionProvider(base.DescriptionProvider):
+    """Provide description for S3 datasets."""
+
+    def __init__(self, api: BaseAPIConsumer):
+        self._api = api
+
+    def get_datagroup_description(self, group: str) -> DataGroupDescription:
+        """
+        Get the description of a data group.
+
+        Parameters
+        ----------
+        group : str
+            The data group name.
+
+        Returns
+        -------
+        DataGroupDescription
+
+        """
+        try:
+            datagroup_metadata = self._api.get_datagroup_metadata(group)
+            display_name = datagroup_metadata["display_name"]
+            description = datagroup_metadata["description"]
+        except base.InvalidDataGroupName:
+            description = ""
+            display_name = group
+        return base.DataGroupDescription(group, description, display_name)
+
+    def get_columns_description(self, dataset: str) -> list[base.ColumnDescription]:
+        """
+        Get the description of the dataset columns.
+
+        Parameters
+        ----------
+        dataset : str
+            The dataset name.
+
+        Returns
+        -------
+        list[ColumnDescription]
+
+        """
+        try:
+            dataset_metadata = self._api.get_dataset_metadata(dataset)
+            cloud_metadata = dataset_metadata[CLOUD_STORAGE]
+            columns = list()
+            for column in cloud_metadata["csv_columns"]:
+                c = base.ColumnDescription(
+                    column["name"], column["data_type"], column["description"]
+                )
+                columns.append(c)
+        except base.InvalidDataSetName:
+            columns = list()
+        return columns
+
+    def get_dataset_description(
+        self, group: str, dataset: str
+    ) -> base.DataSetDescription:
+        """
+        Get the description of a dataset.
+
+        group : str
+            The datagroup name.
+        dataset : str
+            The dataset name.
+
+        Returns
+        -------
+        DatasetDescription
+
+        Raises
+        ------
+        InvalidDataSetName
+
+        """
+        columns = self.get_columns_description(dataset)
+        try:
+            dataset_metadata = self._api.get_dataset_metadata(dataset)
+            display_name = dataset_metadata["display_name"]
+            description = dataset_metadata["long_description"]
+        except base.InvalidDataSetName:
+            display_name = dataset
+            description = ""
+        return base.DataSetDescription(
+            dataset, group, columns, display_name, description
+        )
 
 
-def _normalize_date(date):
-    if isinstance(date, datetime.date):
-        normalized = date
-    elif isinstance(date, str):
-        normalized = utils.yyyymmdd_str_to_date(date)
-    else:
-        msg = f"{date} is not a supported format for date."
-        raise ValueError(msg)
-    return normalized
+class BucketMetadataProvider:
+    """Fetch metadata from S3 datasets."""
+
+    def __init__(self, api: BaseAPIConsumer) -> None:
+        self.api = api
+
+    @lru_cache
+    def get_bucket_group(self, id_: int) -> dict[str, Any]:
+        """
+        Fetch metadata from a bucket group.
+
+        Parameters
+        ----------
+        id_ : int
+            The bucket group id.
+
+        Returns
+        -------
+        dict
+            The bucket group metadata. See the documentation at
+            https://metadata-services.algoseek.com/docs for the
+            `api/v1/public/bucket_group/{bucket_group_id}` for the expected
+            format.
+
+        Raises
+        ------
+        requests.exceptions.HTTPError
+            If a non-existent bucket group id is passed.
+
+        """
+        endpoint = f"public/bucket_group/{id_}/"
+        return self.api.get(endpoint).json()
+
+    def get_dataset_primary_bucket_group(self, text_id: str) -> dict[str, Any]:
+        """
+        Get the metadata from the primary bucket group.
+
+        Parameters
+        ----------
+        text_id : str
+            The dataset text id.
+
+        Returns
+        -------
+        dict
+            The bucket group metadata. See the documentation at
+            https://metadata-services.algoseek.com/docs for the
+            `api/v1/public/bucket_group/{bucket_group_id}` endpoint to see
+            the expected format.
+
+        Raises
+        ------
+        InvalidDatasetName
+            If an non-existent dataset name is passed.
+        ValueError
+            If no primary bucket group is found for the dataset.
+
+        """
+        dataset_metadata = self.api.get_dataset_metadata(text_id)
+        cloud_storage = dataset_metadata[CLOUD_STORAGE]
+        primary_bucket_group = None
+        for bucket_group_id in cloud_storage[BUCKET_GROUPS]:
+            bucket_group_metadata = self.get_bucket_group(bucket_group_id)
+            if bucket_group_metadata["is_primary"]:
+                primary_bucket_group = bucket_group_metadata
+
+        if primary_bucket_group is None:
+            msg = f"No primary bucket group found for dataset {text_id}."
+            raise ValueError(msg)
+
+        return primary_bucket_group
+
+    def get_dataset_bucket_format(self, text_id: str) -> str:
+        """
+        Get the bucket name format.
+
+        Parameters
+        ----------
+        text_id : str
+            The dataset text id.
+
+        Returns
+        -------
+        str
+            a template with the format for the bucket name.
+
+        """
+        bucket_group_metadata = self.get_dataset_primary_bucket_group(text_id)
+        return bucket_group_metadata["bucket_name"]
+
+    def get_dataset_bucket_path_format(self, name: str) -> str:
+        """
+        Get the bucket path format.
+
+        Parameters
+        ----------
+        name : str
+            The dataset name
+
+        Returns
+        -------
+        str
+
+        Raises
+        ------
+        InvalidDatasetName
+            If an non-existent dataset name is passed.
+        """
+        bucket_group_metadata = self.get_dataset_primary_bucket_group(name)
+        return bucket_group_metadata["path_format"]
 
 
-def create_objects_path_template(path: str) -> tuple[str, list[str]]:
-    """
-    Create a template string for object path creation in a S3 bucket.
+class S3DatasetDownloader:
+    """Interface to download datasets stored on S3 buckets."""
 
-    Parameters
-    ----------
-    path : str
-        Path format for the Bucket group.
+    def __init__(
+        self,
+        file_downloader: downloader.FileDownloader,
+        bucket_data_provider: BucketMetadataProvider,
+    ):
+        self.bucket_metadata = bucket_data_provider
+        self.downloader = file_downloader
 
-    Returns
-    -------
-    tuple[str, list[str]]
-        The string with the path template and a list with the placeholder names
-        used.
+    def download(
+        self,
+        text_id: str,
+        download_path: Union[str, Path],
+        date: Union[date_like, tuple[date_like, date_like]],
+        symbols: Union[str, list[str]],
+        expiration_date: Union[date_like, tuple[date_like, date_like], None] = None,
+        n_jobs: Optional[int] = None,
+    ):
+        """
+        Download data from the dataset.
 
-    """
-    prefixes, name = _split_object_path_into_prefixes(path)
-    name_template, name_placeholders = _make_name_template(name)
-    prefix_template, prefix_placeholders = _make_prefix_template(prefixes)
-    if prefix_template:
-        template = f"{prefix_template}/{name_template}"
-    else:
-        template = name_template
+        Parameters
+        ----------
+        text_id : str
+            The dataset text id.
+        download_path : str or pathlib.Path
+            Path to a directory to download dataset files.
+        date : str, datetime.date or tuple
+            Download data in this date range. Dates can be passed as a str with
+            `yyyymmdd` format or as date objects. If a tuple is passed, it is
+            interpreted as a date range and all dates in the closed interval
+            between the two dates are generated. I a single date is passed,
+            download data from this specific date.
+        symbols : str or list[str]
+            Download data associated with these symbols.
+        expiration date : str, datetime.date or tuple
+            Download data with expiration dates in this date range. Dates must
+            be passed used the same format used for the `date` parameter.
+        n_jobs : int or None, default=None
+            The number of jobs created to download files in a parallel manner.
+            If ``None``, a single worker is used.
 
-    placeholders = prefix_placeholders + name_placeholders
-    placeholders = utils.remove_duplicates_preserve_order(placeholders)
-    return template, placeholders
+        """
+        if isinstance(download_path, str):
+            download_path = Path(download_path)
+
+        if not download_path.is_dir():
+            msg = f"{download_path} is not a directory."
+            raise NotADirectoryError(msg)
+
+        filters = downloader.S3KeyFilter(date, symbols, expiration_date)
+        bucket_format = self.bucket_metadata.get_dataset_bucket_format(text_id)
+        bucket_name = downloader.get_bucket_name(bucket_format, *filters.date)
+        bucket = downloader.BucketWrapper(self.downloader.s3, bucket_name)
+
+        path_format = self.bucket_metadata.get_dataset_bucket_path_format(text_id)
+        key_to_size = downloader.create_key_to_size_dictionary(
+            bucket, path_format, filters
+        )
+        total_size = sum(key_to_size.values())
+        if total_size > MAX_DOWNLOAD_SIZE:
+            msg = (
+                f"The total size of the requested data is {total_size}. "
+                f"Maximum allowed is {MAX_DOWNLOAD_SIZE}."
+            )
+            raise DownloadLimitExceededError(msg)
+
+        # TODO: multi process download.
+        keys = list(key_to_size)
+        self.downloader.download(bucket_name, keys, download_path)
 
 
-def _split_object_path_into_prefixes(path: str) -> tuple[list[str], str]:
-    parts = path.split("/")
-    has_prefixes = len(parts) > 1
-    if has_prefixes:
-        prefixes = parts[:-1]
-        name = parts[-1]
-    else:
-        prefixes = list()
-        name = parts[0]
-    return prefixes, name
+class DownloadLimitExceededError(ValueError):
+    """Exception raised when trying to download files above the maximum allowed."""
 
-
-def _make_name_template(name: str) -> tuple[str, list[str]]:
-    parts = name.split(".")
-
-    if parts[-1] in ["zip", "gz"]:
-        extension = ".".join(parts[-2:])
-        name_parts = parts[:-2]
-    else:
-        extension = parts[-1]
-        name_parts = parts[:-1]
-
-    name_placeholders = [x for x in name_parts if x in PlaceHolders.ALL]
-    name_parts = [f"{{{x}}}" if x in PlaceHolders.ALL else x for x in name_parts]
-    name_template = ".".join(name_parts) + f".{extension}"
-    return name_template, name_placeholders
-
-
-def _make_prefix_template(prefixes: list[str]) -> tuple[str, list[str]]:
-    prefixes_placeholders = [x for x in prefixes if x in PlaceHolders.ALL]
-    prefixes = [f"{{{x}}}" if x in PlaceHolders.ALL else x for x in prefixes]
-    prefix_template = "/".join(prefixes)
-    return prefix_template, prefixes_placeholders
+    pass
