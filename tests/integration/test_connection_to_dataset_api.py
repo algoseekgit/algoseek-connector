b8a007e6f78485d5b284ca09c8b4974c99c462e0
Gabriel Riquelme <griquelme.chm@gmail.com>
2024-05-08 15:23:09 -0300

    feat: Add DatasetAPIProvider.

---
+++ tests/integration/test_connection_to_dataset_api.py
@@ -0,0 +1,69 @@
+import os
+from unittest import mock
+
+import pytest
+import requests
+from algoseek_connector.base import InvalidDataGroupName
+from algoseek_connector.dataset_api import BearerAuth, DatasetAPIProvider
+from algoseek_connector.settings import load_settings
+
+
+@pytest.fixture
+def mock_env():
+    with mock.patch.dict(os.environ, clear=True):
+        yield
+
+
+class TestBearerAuth:
+    def test_token_is_none_if_no_email(self, mock_env):
+        os.environ["ALGOSEEK__DATASET_API__PASSWORD"] = "mock_pass"
+        config = load_settings().dataset_api
+        auth = BearerAuth(config)
+        assert auth.token is None
+
+    def test_token_is_none_if_no_password(self, mock_env):
+        os.environ["ALGOSEEK__DATASET_API__EMAIL"] = "mock@email.com"
+        config = load_settings().dataset_api
+        auth = BearerAuth(config)
+        assert auth.token is None
+
+    def test_invalid_credentials_raise_error(self, mock_env):
+        os.environ["ALGOSEEK__DATASET_API__EMAIL"] = "mock@email.com"
+        os.environ["ALGOSEEK__DATASET_API__PASSWORD"] = "mock_pass"
+        config = load_settings().dataset_api
+        with pytest.raises(requests.HTTPError):
+            BearerAuth(config)
+
+    def test_login_ok(self):
+        # uses credentials defined in the environment
+        config = load_settings().dataset_api
+        BearerAuth(config)
+
+
+class TestDatasetAPI:
+    @pytest.fixture(scope="class")
+    def api(self):
+        return DatasetAPIProvider()
+
+    def test_list_data_groups(self, api: DatasetAPIProvider):
+        groups = api.list_data_groups()
+        assert len(groups)
+        assert all(isinstance(x, str) for x in groups)
+
+    def test_get_data_group_ok(self, api: DatasetAPIProvider):
+        for group in api.list_data_groups():
+            api.get_data_group(group)
+
+    def test_get_data_group_invalid_group_name_raises_error(self, api: DatasetAPIProvider):
+        with pytest.raises(InvalidDataGroupName):
+            api.get_data_group("invalid-group-name")
+
+    def test_list_dataset_destinations_ok(self, api: DatasetAPIProvider):
+        datasets = api.list_dataset_destinations()
+        assert len(datasets)
+        assert all(isinstance(x, int) for x in datasets)
+
+    def test_get_dataset_destinations_ok(self, api: DatasetAPIProvider):
+        destinations = api.list_dataset_destinations()
+        for destination_id in destinations:
+            api.get_dataset(destination_id)
