f87a4e248de7ca411c6d6f7fe732ce9b6dec1899
Gabriel Riquelme <griquelme.chm@gmail.com>
2024-05-15 12:08:56 -0300

    refactor: Delete constants module.

--- src/algoseek_connector/utils.py
+++ src/algoseek_connector/utils.py
@@ -6,8 +6,6 @@ import hashlib
 import re
 from pathlib import Path
 
-from . import constants
-
 
 class ExpirationMonthCode(enum.Enum):
     """Represent Expiration month codes for futures."""
@@ -104,4 +102,4 @@ def sha1_digest(path: Path) -> str:
 
 def get_algoseek_path() -> Path:
     """Get the path to the algoseek directory located in the user home."""
-    return Path.home() / constants.ALGOSEEK_DIR
+    return Path.home() / ".algoseek"
