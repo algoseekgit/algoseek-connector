f2702ee7918c95bd3703ed39e8dade72b6c17ba1
Gabriel Riquelme <griquelme.chm@gmail.com>
2024-05-24 16:41:20 -0300

    build: Add autodoc-pydantic to docs dependencies.

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ extensions = [
     "sphinx_design",
     "sphinx_toggleprompt",
     "numpydoc",
+    "sphinxcontrib.autodoc_pydantic",
 ]
 
 intersphinx_mapping = {
@@ -38,3 +39,18 @@ html_theme_options = {
     "sidebar_width": "300px",
     "page_width": "1000px",
 }
+
+# autodoc-pydantic
+# ----------------
+autodoc_pydantic_model_show_json = False
+autodoc_pydantic_model_show_field_summary = False
+autodoc_pydantic_model_show_config_summary = False
+autodoc_pydantic_model_undoc_members = False
+autodoc_pydantic_field_list_validators = False
+autodoc_pydantic_field_show_constraints = False
+
+autodoc_pydantic_settings_show_json = False
+autodoc_pydantic_settings_show_config_summary = False
+autodoc_pydantic_settings_show_field_summary = False
+autodoc_pydantic_settings_show_validator_members = False
+autodoc_pydantic_settings_show_validator_summary = False
